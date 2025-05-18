53b26783a0f72012bf1be7c87a3a5f6913cd58fe
i-ankityadav <ankitofficial0271@gmail.com>
2025-05-18 20:07:06 +0530

    User subscriber count and other fields added successfully

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -287,3 +287,72 @@ export const updateUserCoverImage = asyncHandler(async (req,res)=>{
     return res.status(200)
         .json(new ApiResponse(200,user,"CoverImage update successfully"))
 })
+
+//user profile starts here
+
+export const getUserChannelProfile = asyncHandler(async (req,res)=>{
+    const {username} = req.params;
+    if (!username?.trim()){
+        throw new ApiError(400,"usermame is missing")
+    }
+
+   // User.find({username})
+ const channel = await  User.aggregate([{
+     $match:{
+         username:username?.toLowerCase()
+     }},{
+     $lookup:{
+         from:"subscriptions",
+         localField: "_id",
+         foreignField:"channel",
+         as:"subscribers"
+     }},
+     {
+     $lookup:{
+         from:"subscriptions",
+         localField: "_id",
+         foreignField:"channel",
+         as:"subscribed"
+     }
+     },
+     {
+         $addFields:{
+             subscribersCount : {
+                 $size: "$subscribers"
+             },
+             channelIsSubscribedToCount: {
+                 $size: "$subscribe"
+             },
+             isSubscribed:{
+                 $cond: {
+                     if:{$in:[req.user?._id,"$subscribers.subscribers"]},
+                     then:true,
+                     else:false
+                 }
+             }
+         }
+     },
+     {
+         $project:{
+             fullName:1,
+             username:1,
+             subscribersCount:1,
+             channelIsSubscribedToCount:1,
+             avatar:1,
+             isSubscribed:1,
+             coverImage:1,
+         }
+     }
+
+ ])
+
+if (!channel?.length){
+    throw new ApiError(404,"Channel does not exist");
+}
+
+return res.status(200)
+    .json(new ApiResponse(200,channel[0]),"User channel fetched successfully")
+})
+
+
+
